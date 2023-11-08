c8f3b17a574b8f22e10fea424c4a0a4d047d426f
Ahyoung-Kim <eileel@naver.com>
2023-11-08 20:10:27 +0900

    :art: Hotfix
    hotfix

--- src/components/molecule/section/projects/ProjectCard.jsx
+++ src/components/molecule/section/projects/ProjectCard.jsx
@@ -1,5 +1,8 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 import ProjectDescription from "../../contents/ProjectDescription";
 import GrayLine from "../../../atomic/contents/GrayLine";
@@ -9,17 +12,44 @@ import MonitorImage from "../../../atomic/contents/MonitorImage";
 import { PROJECTS_URL, PUBLIC_URL } from "../../../../common/config";
 import { useMediaQueries } from "../../../layout/MediaQuery";
 import { useNavigate } from "react-router-dom";
+import colors from "../../../../common/colors";
 
 const ProjectImage = ({ src, mobile }) => {
   const { isPc } = useMediaQueries();
+  const [hover, setHover] = useState(-1);
 
   return (
-    <>
+    <ImageWrapper
+      style={mobile ? { width: "35%" } : isPc ? { width: "70%" } : {}}
+      onMouseEnter={() => setHover(1)}
+      onMouseLeave={() => setHover(0)}
+    >
       <Image
-        style={mobile ? { width: "35%" } : isPc ? { width: "70%" } : {}}
         src={src ? `${PROJECTS_URL}/${src}` : `${PUBLIC_URL}/img/page-pc.png`}
       />
-    </>
+
+      {isPc ? (
+        <DetailDiv
+          className={hover === 1 ? "fadeIn" : hover === 0 ? "fadeOut" : "none"}
+        >
+          <DetailButton className={hover === 1 && "move"}>
+            README
+            <FontAwesomeIcon
+              icon={faInfoCircle}
+              style={{ marginLeft: "0.5rem" }}
+            />
+          </DetailButton>
+        </DetailDiv>
+      ) : (
+        <DetailButton style={{ margin: "0.5rem auto 0" }}>
+          README
+          <FontAwesomeIcon
+            icon={faInfoCircle}
+            style={{ marginLeft: "0.5rem" }}
+          />
+        </DetailButton>
+      )}
+    </ImageWrapper>
   );
 };
 
@@ -56,14 +86,99 @@ const Container = styled.div`
 const ImageDiv = styled.div`
   width: 100%;
   text-align: center;
+  margin-bottom: 1rem;
   // background-color: orange;
+`;
+
+const ImageWrapper = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  // background-color: aqua;
+  border-radius: 0.6rem;
+  position: relative;
+  overflow: hidden;
+  cursor: pointer;
 `;
 
 const Image = styled.img`
   width: 100%;
-  margin-bottom: 1rem;
   object-fit: contain;
   display: inline-block;
   border: 0.6rem solid black;
-  border-radius: 0.6rem;
+  border-radius: inherit;
+`;
+
+const fadeIn = keyframes`
+0% {
+  opacity: 0;
+}
+100% {
+  opacity: 1;
+}
+`;
+
+const fadeOut = keyframes`
+0% {
+  opacity: 1;
+}
+100% {
+  opacity: 0;
+}
+`;
+
+const DetailDiv = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+
+  &.fadeIn {
+    animation: ${fadeIn} 1s ease forwards;
+  }
+
+  &.fadeOut {
+    animation: ${fadeOut} 1s ease;
+  }
+`;
+
+const move = keyframes`
+0% {
+  transform: translateY(15%);
+}
+
+25% {
+  transform: translateY(-30%);
+}
+
+50% {
+  transform: translateY(5%);
+}
+
+75% {
+  transform: translateY(-10%);
+}
+
+100% {
+  transform: translateY(0);
+}
+`;
+
+const DetailButton = styled.div`
+  padding: 0.4rem 0.8rem;
+  border-radius: 0.5rem;
+  // background-color: black;
+  background: linear-gradient(180deg, #f048c1 25.5%, #b200cf 84.5%);
+  color: white;
+  font-weight: bold;
+  width: max-content;
+
+  &.move {
+    animation: ${move} 1s ease;
+  }
 `;
